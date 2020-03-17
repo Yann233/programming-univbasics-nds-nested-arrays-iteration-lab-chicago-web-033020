acfc424ca5e879ef0ce80e6f121c679ac5c9c228
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-03-17 17:45:09 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -27,7 +27,7 @@ def find_greater_pair(src)
   newArray = []#先设一个新array
     row_index = 0 #这个是index 或者 i
     while row_index < src.length do #src.length shows how many elements in the src array
-      #如果第一个>或=第二个数，把第一个数插入newArray
+      #如果第一个>或=第二个数，把第一个数从最后插入newArray
       newArray.push(src[row_index][0] >= src[row_index][1] ?    src[row_index][0] :   src[row_index][1])
       #                                    如果第0位>第1位 ?     True就插入第0位   ：    False就插入第1位
 =begin
